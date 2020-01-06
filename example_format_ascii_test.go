a7d8dd7df4b73d0267a1dec7cc83edae291fc727
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-01-06 14:00:56 -0800

    examples

--- example_format_ascii_test.go
+++ example_format_ascii_test.go
@@ -53,6 +53,36 @@ func ExampleType_Format_ascii() {
 	fmt.Print("U+002F (/)   SOLIDUS\n\n")
 	fmt.Printf("%z\n", font8x8.U002F)
 
+	fmt.Print("U+0030 (0)   ARABIC DIGIT ZERO\n\n")
+	fmt.Printf("%z\n", font8x8.U0030)
+
+	fmt.Print("U+0031 (1)   ARABIC DIGIT ONE\n\n")
+	fmt.Printf("%z\n", font8x8.U0031)
+
+	fmt.Print("U+0032 (2)   ARABIC DIGIT TWO\n\n")
+	fmt.Printf("%z\n", font8x8.U0032)
+
+	fmt.Print("U+0033 (3)   ARABIC DIGIT THREE\n\n")
+	fmt.Printf("%z\n", font8x8.U0033)
+
+	fmt.Print("U+0034 (4)   ARABIC DIGIT FOUR\n\n")
+	fmt.Printf("%z\n", font8x8.U0034)
+
+	fmt.Print("U+0035 (5)   ARABIC DIGIT FIVE\n\n")
+	fmt.Printf("%z\n", font8x8.U0035)
+
+	fmt.Print("U+0036 (6)   ARABIC DIGIT SIX\n\n")
+	fmt.Printf("%z\n", font8x8.U0036)
+
+	fmt.Print("U+0037 (7)   ARABIC DIGIT SEVEN\n\n")
+	fmt.Printf("%z\n", font8x8.U0037)
+
+	fmt.Print("U+0038 (8)   ARABIC DIGIT EIGHT\n\n")
+	fmt.Printf("%z\n", font8x8.U0038)
+
+	fmt.Print("U+0039 (9)   ARABIC DIGIT NINE\n\n")
+	fmt.Printf("%z\n", font8x8.U0039)
+
 	// Output:
 	//
 	// U+0021 (!)   EXPLAMATION MARK
@@ -218,5 +248,115 @@ func ExampleType_Format_ascii() {
 	// ░██░░░░░
 	// ██░░░░░░
 	// █░░░░░░░
+	// ░░░░░░░░
+	//
+	// U+0030 (0)   ARABIC DIGIT ZERO
+	//
+	// ░█████░░
+	// ██░░░██░
+	// ██░░███░
+	// ██░████░
+	// ████░██░
+	// ███░░██░
+	// ░█████░░
+	// ░░░░░░░░
+	//
+	// U+0031 (1)   ARABIC DIGIT ONE
+	//
+	// ░░██░░░░
+	// ░███░░░░
+	// ░░██░░░░
+	// ░░██░░░░
+	// ░░██░░░░
+	// ░░██░░░░
+	// ██████░░
+	// ░░░░░░░░
+	//
+	// U+0032 (2)   ARABIC DIGIT TWO
+	//
+	// ░████░░░
+	// ██░░██░░
+	// ░░░░██░░
+	// ░░███░░░
+	// ░██░░░░░
+	// ██░░██░░
+	// ██████░░
+	// ░░░░░░░░
+	//
+	// U+0033 (3)   ARABIC DIGIT THREE
+	//
+	// ░████░░░
+	// ██░░██░░
+	// ░░░░██░░
+	// ░░███░░░
+	// ░░░░██░░
+	// ██░░██░░
+	// ░████░░░
+	// ░░░░░░░░
+	//
+	// U+0034 (4)   ARABIC DIGIT FOUR
+	//
+	// ░░░███░░
+	// ░░████░░
+	// ░██░██░░
+	// ██░░██░░
+	// ███████░
+	// ░░░░██░░
+	// ░░░████░
+	// ░░░░░░░░
+	//
+	// U+0035 (5)   ARABIC DIGIT FIVE
+	//
+	// ██████░░
+	// ██░░░░░░
+	// █████░░░
+	// ░░░░██░░
+	// ░░░░██░░
+	// ██░░██░░
+	// ░████░░░
+	// ░░░░░░░░
+	//
+	// U+0036 (6)   ARABIC DIGIT SIX
+	//
+	// ░░███░░░
+	// ░██░░░░░
+	// ██░░░░░░
+	// █████░░░
+	// ██░░██░░
+	// ██░░██░░
+	// ░████░░░
+	// ░░░░░░░░
+	//
+	// U+0037 (7)   ARABIC DIGIT SEVEN
+	//
+	// ██████░░
+	// ██░░██░░
+	// ░░░░██░░
+	// ░░░██░░░
+	// ░░██░░░░
+	// ░░██░░░░
+	// ░░██░░░░
+	// ░░░░░░░░
+	//
+	// U+0038 (8)   ARABIC DIGIT EIGHT
+	//
+	// ░████░░░
+	// ██░░██░░
+	// ██░░██░░
+	// ░████░░░
+	// ██░░██░░
+	// ██░░██░░
+	// ░████░░░
+	// ░░░░░░░░
+	//
+	// U+0039 (9)   ARABIC DIGIT NINE
+	//
+	// ░████░░░
+	// ██░░██░░
+	// ██░░██░░
+	// ░█████░░
+	// ░░░░██░░
+	// ░░░██░░░
+	// ░███░░░░
 	// ░░░░░░░░
 }
