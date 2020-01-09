3ef06091602cac572bea38d4c184651a068b07b4
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-01-08 19:40:31 -0800

    examples

---
+++ example_format_u2574_test.go
@@ -0,0 +1,23 @@
+package font8x8_test
+
+import (
+	"github.com/reiver/go-font8x8"
+
+	"fmt"
+)
+
+func ExampleType_Format_u2574() {
+
+	fmt.Printf("%z\n", font8x8.U2574)
+
+	// Output:
+	//
+	// ░░░░░░░░
+	// ░░░░░░░░
+	// ░░░░░░░░
+	// ░░░░░░░░
+	// ████░░░░
+	// ░░░░░░░░
+	// ░░░░░░░░
+	// ░░░░░░░░
+}
